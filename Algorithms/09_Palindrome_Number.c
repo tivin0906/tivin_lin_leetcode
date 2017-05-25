0128b145849bc856b9e6ba5a6f685c02cbf14e80
Tivin_Lin <tivin_lin@htc.com>
2017-05-25 20:40:57 +0800

    [Add] #009 Palindrome Number;  [Modify] #007 Add comment

--- Algorithms/09_Palindrome_Number.c
+++ Algorithms/09_Palindrome_Number.c
@@ -14,7 +14,7 @@ int power(int x, int num)
 int reverse(int x) 
 {   
     int i=1, sum=0, tmp=x;
-	
+    
     // the length of an integer, e.g. 123 -> 3
     int d = ( x == 0 ? 1 : ((int)(log10(fabs(x))+1)) );
 
@@ -24,8 +24,23 @@ int reverse(int x)
         x = x/10;
         i++;
     }
-
+    
     // check result is overflow or not
-	sum = ( (tmp%10) == (sum/power(10,(d-1))) ? sum : 0 );
+	sum = ( (tmp%10) == (sum/power(10,(d-1))) ? sum : 0 );  
+	
     return sum;
+}
+
+bool isPalindrome(int x)
+{
+    if(x<0)
+        return false;
+        
+	int revx;
+    revx = reverse(x);
+
+	if(x == revx)
+		return true;
+	else
+		return false;
 }
